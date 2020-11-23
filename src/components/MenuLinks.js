622cc43ecb4e50171bf67830060da3660857a991
apokalipsa44 <m_lachowicz@wp.pl>
2020-11-23 23:18:44 +0100

    menu items added added

---
+++ src/components/MenuLinks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MenuLinks() {
+  return (
+    <div>
+      <ul>
+        <li>link 1</li>
+        <li>link 2</li>
+        <li>link 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuLinks;
