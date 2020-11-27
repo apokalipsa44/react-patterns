f6fd75b1a4c8bd02b89680cecd8db384504dbdea
apokalipsa44 <m_lachowicz@wp.pl>
2020-11-27 12:43:15 +0100

    useEffect view definition and code sample

--- src/views/hooks/useEffectView.js
+++ src/views/hooks/useEffectView.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Box, Container, Typography } from "@material-ui/core";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
+import { lioshi } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 function useEffectView() {
+  SyntaxHighlighter.registerLanguage("javascript", js);
+  var basicHookString =
+    "useEffect(() => {\n" +
+    "      // code to be executed\n" +
+    "      // axios.get('/user?name=donpiano')\n" +
+    "  return () => {\n" +
+    "     // cleanup function (unsubscribe or unmount)\n" +
+    "  }, [modifiedVariable]);";
   return (
     <Container>
-      useEffectViewffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+      <Typography>
+        I'ts basicly comopnentDidMount for function components. Function will
+        run the code every time component get's re-rendered (if we leave second
+        parameter empty), or variables passed to second parameter gets changed.
+        <Box>
+          <SyntaxHighlighter style={lioshi}>
+            {basicHookString}
+          </SyntaxHighlighter>
+        </Box>
+      </Typography>
     </Container>
   );
 }
