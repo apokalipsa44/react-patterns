ee51a5e2f2ca66cbda3550a6e1c92a951924a527
apokalipsa44 <m_lachowicz@wp.pl>
2020-12-14 14:45:14 +0100

    heroku deployment

--- src/App.js
+++ src/App.js
@@ -7,30 +7,30 @@ import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Switch from "@material-ui/core/Switch";
 import { CssBaseline, FormControlLabel, Grid } from "@material-ui/core";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import ReactGA from "react-ga";
+// import ReactGA from "react-ga";
 
 const useStyles = makeStyles({
   appBar: {
     height: "120px",
   },
   darkModeSwitch: {
-    border: '3px solid rgba(0,0,0,0.15);',
-    borderRadius:'15px',
+    border: "3px solid rgba(0,0,0,0.18);",
+    borderRadius: "15px",
     zIndex: 1280,
-    margin: "12px",
-    color: "inherit",
+    margin: "4px",
     position: "fixed",
-    right: "16px",
+    right: "1px",
+    color: "#f0f6ff",
   },
 });
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize("G-JQVL1STPCF");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-  // ReactGA.initialize("G-JQVL1STPCF");
-  // ReactGA.pageview(window.location.pathname + window.location.search);
+  // useEffect(() => {
+  //   ReactGA.initialize("G-JQVL1STPCF");
+  //   ReactGA.pageview('/')
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
+ 
   const [darkState, setDarkState] = useState(true);
   const palletType = darkState ? "dark" : "light";
   const darkTheme = createMuiTheme({
